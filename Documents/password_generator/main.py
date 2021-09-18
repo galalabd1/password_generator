70aedc8024029dc81466a602e4a7070930d60e09
Abdalaziz Galal <a.galal@mail.utoronto.ca>
2021-09-18 16:27:46 -0400

    adding atleast 1 char

--- Documents/password_generator/main.py
+++ Documents/password_generator/main.py
@@ -1,19 +1,37 @@
+from tkinter import *
+
 import random
 import string
-
+import numpy as np
 
 #characters used to generate our password
-chars = string.ascii_letters + string.digits + "@#$%^&*"
+alphabet = string.ascii_letters
+numbers = string.digits
+s_chars = "@#$%^&*"
 
 def generate_password():
+    length = int(input("Enter a length of atleast 8"))
+
+    ## generating a list so atleast one special character and number selected
+
+    numbers_count = random.randint(1, length//2)
+    schars_count = random.randint(1, length//2)
+    alphabets_count = length - numbers_count - schars_count
+
+    #generating password
     password = []
-    length = int(input("Enter the required length for password"))
-    for i in range(length):
-        password.append(random.choice(chars))
+    for i in range(alphabets_count):
+        password.append(random.choice(alphabet))
+
+    for i in range(numbers_count):
+        password.append(random.choice(numbers))
+
+    for i in range(schars_count):
+        password.append(random.choice(s_chars))
+
 
     random.shuffle(password)
     string_password = ''.join(password)
     print(string_password)
 
 generate_password()
-
